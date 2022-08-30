bdf8ced206f104a06713f766bb31601b3da0bfd2
sarah <sarah@mobidoo.co.kr>
2022-08-30 23:58:33 +0900

    feat: 메인페이지 소개 섹션 및 푸터 구현

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,22 @@
 import React, { FunctionComponent } from 'react'
-import { Link } from 'gatsby'
+import styled from '@emotion/styled'
+import GlobalStyle from 'components/Common/GlobalStyle'
+import Footer from 'components/Common/Footer'
+import Introduction from 'components/Main/Introduction'
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+`
 
 const IndexPage: FunctionComponent = function () {
   return (
-    <div>
-      <Link to="/info/">To Info</Link>
-    </div>
+    <Container>
+      <GlobalStyle />
+      <Introduction />
+      <Footer />
+    </Container>
   )
 }
 
